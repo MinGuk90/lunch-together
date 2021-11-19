83e58e2b7a76e586210e345e28c06f47019003c5
minguk <minkuk90@gmail.com>
2021-11-19 23:16:07 +0900

    [feat] : reactreveal

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,11 +2,27 @@ import React from "react";
 import styled from "styled-components";
 import { Tab } from "../components/Tab";
 import { Tabpac } from "../components/Tabpac";
+import Zoom from "react-reveal/Zoom";
+import Fade from "react-reveal/Fade";
+
 const Home = () => {
   return (
     <CardWrap>
       {/* <Tab /> */}
       <Tabpac />
+      <input />
+      <Zoom>
+        <Box />
+        <Box />
+        <Box />
+      </Zoom>
+      <Fade left>
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+      </Fade>
     </CardWrap>
   );
 };
@@ -15,4 +31,10 @@ const CardWrap = styled.div`
   height: 2000px;
 `;
 
+const Box = styled.div`
+  height: 100px;
+  width: 100px;
+  background-color: red;
+  margin: 500px;
+`;
 export default Home;
